09b5d0b9f58577a717ec216df36501513d71e904
2DAM <2DAM@2DAM-19>
2023-09-25 09:17:32 +0200

    9:17 25/09

--- Eurovision/src/main/java/Utilities/Mapas.java
+++ Eurovision/src/main/java/Utilities/Mapas.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package Utilities;
 
 import java.util.Map;
@@ -18,10 +15,13 @@ public class Mapas
 	{
 		
 		Map<String,Coche> mapaDeCoches = new TreeMap<String,Coche>();
+		
 		Coche nuevoUno = new Coche("7711LPQ","Seat");
 		Coche nuevoDos = new Coche("5566SQL","Megane");
+		
 		mapaDeCoches.put(nuevoUno.getMatricula(), nuevoUno);
 		mapaDeCoches.put(nuevoDos.getMatricula(), nuevoDos);
+		
 		System.out.println(mapaDeCoches);
 		
 	}
